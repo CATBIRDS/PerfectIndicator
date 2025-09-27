9ac09fac6f8db3a26e9255eef6942717c42d899b
catbirds <CATBIRDS@users.noreply.github.com>
2025-09-27 11:14:00 -0400

    PerfectIndicator 1.0.2
    Updated widget recoloring code and other similar code references to handle the new Stamina widget

--- scripts/mods/PerfectIndicator/PerfectIndicator.lua
+++ scripts/mods/PerfectIndicator/PerfectIndicator.lua
@@ -5,7 +5,7 @@ local AttackSettings = require("scripts/settings/damage/attack_settings")
 local VisualLoadoutCustomization = require("scripts/extension_systems/visual_loadout/utilities/visual_loadout_customization")
 -- HudElementBlocking Dependencies
 local UIWidget = require("scripts/managers/ui/ui_widget")
-local HudElementBlockingSettings = require("scripts/ui/hud/elements/blocking/hud_element_blocking_settings")
+local HudElementStaminaSettings = require("scripts/ui/hud/elements/blocking/hud_element_stamina_settings")
 
 local MOD = {
     ENABLED = true,             -- Enable or disable the mod
@@ -168,76 +168,123 @@ end)
 --  ╚═╝ ╩ ╩ ╩╩ ╩╩╝╚╝╩ ╩
 
 -- Stamina Bar Modification - changes widget to be green when perfect blocking. Unfortunately long hook as coloring takes place at the end but hook_safe is insufficient
-mod:hook(CLASS.HudElementBlocking, "_draw_shields", function(func, self, dt, t, ui_renderer)
+mod:hook(CLASS.HudElementStamina, "_draw_stamina_chunks", function(func, self, dt, t, ui_renderer)
     if not MOD.ENABLED then return func(self, dt, t, ui_renderer) end
     if MOD.STAMINA then
-        local num_shields = self._shield_amount
-        if num_shields < 1 then
+        local num_stamina_chunks = self._num_stamina_chunks
+
+        if num_stamina_chunks < 1 then
             return
         end
-        local widget = self._shield_widget
-        local widget_offset = widget.offset
-        local shield_width = self._shield_width
+
+        local stamina_chunk_width = self._stamina_chunk_width
+        local bar_size = HudElementStaminaSettings.bar_size
         local stamina_fraction = 1
         local parent = self._parent
         local player_extensions = parent:player_extensions()
         local is_perfect_blocking
+
         if player_extensions then
             local player_unit_data = player_extensions.unit_data
+
             if player_unit_data then
                 local stamina_component = player_unit_data:read_component("stamina")
                 local block_component = player_unit_data:read_component("block")
+                
                 is_perfect_blocking = block_component and block_component.is_blocking and block_component.is_perfect_blocking
+
                 if stamina_component and stamina_component.current_fraction then
                     stamina_fraction = stamina_component.current_fraction
                 end
             end
         end
-        local gauge_widget = self._widgets_by_name.gauge
-        local fake_percent = math.clamp(stamina_fraction, 0, 1) * 100 -- For some reason calculated properly but not used for proper display in base game
-        gauge_widget.content.value_text = string.format("%.0f%%", fake_percent)
-        local step_fraction = 1 / num_shields
-        if self._start_on_half_bar then
-            stamina_fraction = stamina_fraction - step_fraction * 0.51
+
+        if stamina_fraction <= 0 and self._last_stamina_fraction > 0 then
+            self:_start_animation("on_stamina_depleted", self._widgets_by_name, 1)
         end
-        local spacing = HudElementBlockingSettings.spacing
-        local x_offset = (shield_width + spacing) * (num_shields - 1) * 0.5
-        local shields = self._shields
-        for i = num_shields, 1, -1 do
-            local shield = shields[i]
-            if not shield then
-                return
+
+        local stamina_spent = self._last_stamina_fraction - stamina_fraction
+        local spent_stamina_animation = self._spent_stamina_animation
+
+        if stamina_spent > HudElementStaminaSettings.stamina_spent_threshold and not spent_stamina_animation.running then
+            spent_stamina_animation.running = true
+            spent_stamina_animation.start_t = t + HudElementStaminaSettings.stamina_spent_delay
+            spent_stamina_animation.starting_stamina = self._last_stamina_fraction
+            spent_stamina_animation.stamina = self._last_stamina_fraction
+        end
+
+        self._last_stamina_fraction = stamina_fraction
+
+        local gauge_widget = self._widgets_by_name.gauge
+        local stamina_text_style = gauge_widget.style.value_text
+        local stamina_text_color = stamina_text_style.text_color
+
+        stamina_text_color[1] = self._show_stamina_percentage_text and 255 or 0
+        gauge_widget.content.value_text = string.format("%.0f%%", math.clamp(stamina_fraction, 0, 1) * 100)
+
+        local spacing = HudElementStaminaSettings.spacing
+        local stamina_bar_widget = self._widgets_by_name.stamina_bar
+
+        -- Apply perfect block coloring to stamina bar
+        if is_perfect_blocking then
+            local bar_fill_color = stamina_bar_widget.style.bar_fill.color
+            bar_fill_color[1] = GREEN.full[1]
+            bar_fill_color[2] = GREEN.full[2]
+            bar_fill_color[3] = GREEN.full[3]
+            bar_fill_color[4] = GREEN.full[4]
+        else
+            local bar_fill_color = stamina_bar_widget.style.bar_fill.color
+            bar_fill_color[1] = OG.full[1]
+            bar_fill_color[2] = OG.full[2]
+            bar_fill_color[3] = OG.full[3]
+            bar_fill_color[4] = OG.full[4]
+        end
+
+        stamina_bar_widget.style.bar_fill.size_addition[1] = -(bar_size[1] * (1 - stamina_fraction))
+
+        if spent_stamina_animation.running then
+            if stamina_fraction >= spent_stamina_animation.stamina then
+                spent_stamina_animation.running = false
             end
-            local end_value = i * step_fraction
-            local start_value = end_value - step_fraction
-            local is_full, is_half, is_empty
-            if fake_percent == 100 then -- Fixes base-game bug where final shield is never colored "full" even at max stamina
-                is_full = true
-            elseif stamina_fraction >= start_value + step_fraction * 0.5 then
-                is_full = true
-            elseif start_value < stamina_fraction then
-                is_half = true
+
+            if t >= spent_stamina_animation.start_t then
+                spent_stamina_animation.stamina = spent_stamina_animation.stamina - dt * HudElementStaminaSettings.stamina_spent_drain_speed
+            end
+
+            stamina_bar_widget.style.bar_spent.size_addition[1] = -(bar_size[1] * (1 - spent_stamina_animation.stamina))
+        else
+            stamina_bar_widget.style.bar_spent.size_addition[1] = -(bar_size[1] * (1 - stamina_fraction))
+        end
+
+        local show_last_nodge = num_stamina_chunks - math.floor(num_stamina_chunks) > 0
+        local num_stamina_chunks_to_show = show_last_nodge and num_stamina_chunks or num_stamina_chunks - 1
+        local stamina_nodge_widget = self._stamina_nodge_widget
+        local stamina_nodge_widget_offset = stamina_nodge_widget.offset
+        local stamina_nodge_widget_style = stamina_nodge_widget.style.nodges
+        local stamina_nodge_widget_color = stamina_nodge_widget_style.color
+        local stamina_nodge_offset = stamina_chunk_width
+
+        for i = 1, num_stamina_chunks_to_show do
+            stamina_nodge_widget_offset[1] = stamina_nodge_offset
+
+            local is_nodge_above_current_stamina = i > stamina_fraction * num_stamina_chunks
+            
+            -- Apply perfect block coloring to stamina nodges
+            local nodge_color
+            if is_perfect_blocking then
+                nodge_color = is_nodge_above_current_stamina and GREEN.empty or GREEN.half
             else
-                is_empty = true
+                nodge_color = is_nodge_above_current_stamina and OG.empty or OG.half
             end
-            local active_color
-            -- Color in accordance to perfect blocking state and shield fullness
-            if is_empty then
-                active_color = is_perfect_blocking and GREEN.empty or OG.empty
-            elseif is_full then
-                active_color = is_perfect_blocking and GREEN.full or OG.full
-            elseif is_half then
-                active_color = is_perfect_blocking and GREEN.half or OG.half
-            end
-            local widget_style = widget.style
-            local widget_color = widget_style.full.color
-            widget_color[1] = active_color[1]
-            widget_color[2] = active_color[2]
-            widget_color[3] = active_color[3]
-            widget_color[4] = active_color[4]
-            widget_offset[1] = x_offset
-            UIWidget.draw(widget, ui_renderer)
-            x_offset = x_offset - shield_width - spacing
+
+            stamina_nodge_widget_color[1] = nodge_color[1]
+            stamina_nodge_widget_color[2] = nodge_color[2]
+            stamina_nodge_widget_color[3] = nodge_color[3]
+            stamina_nodge_widget_color[4] = nodge_color[4]
+
+            UIWidget.draw(stamina_nodge_widget, ui_renderer)
+
+            stamina_nodge_offset = stamina_nodge_offset + (stamina_chunk_width + spacing)
         end
     else
         return func(self, dt, t, ui_renderer)
@@ -245,7 +292,7 @@ mod:hook(CLASS.HudElementBlocking, "_draw_shields", function(func, self, dt, t, 
 end)
 
 -- Override visibility function while PERSISTENT_STAMINA is enabled
-mod:hook(CLASS.HudElementBlocking, "_update_visibility", function(func, self, dt, t)
+mod:hook(CLASS.HudElementStamina, "_update_visibility", function(func, self, dt, t)
     if not MOD.ENABLED then return func(self, dt, t, input_service, ui_renderer, render_settings) end
     if MOD.PERSISTENT_STAMINA then
         self._alpha_multiplier = 1
